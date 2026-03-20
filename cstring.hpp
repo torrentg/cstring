292897fc7f97fc7b8bfaac55643b100dfb9150ba
Gerard Torrent <gerard@generacio.com>
2026-03-20 12:43:17 +0100

    Fixed concurrency issue

--- cstring.hpp
+++ cstring.hpp
@@ -207,15 +207,7 @@ class basic_cstring
             return;
 
         atomic_prefix_type *ptr = get_ptr_to_counter(str);
-        prefix_type counts = ptr[0].load(std::memory_order_relaxed);
-
-        if (counts == 0) // constant (eg. m_empty)
-            return;
-
-        if (counts > 1)
-            counts = ptr[0].fetch_sub(1, std::memory_order_relaxed);
-
-        if (counts == 1)
+        if (ptr[0].fetch_sub(1, std::memory_order_acq_rel) == 1)
             deallocate(str);
     }
 
